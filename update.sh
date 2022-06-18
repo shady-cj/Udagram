51a2d818a2ac0df309bbd8ea75cd16c0db93aaac
shady-cj <petersp2000@gmail.com>
2022-06-18 14:43:19 +0100

    adding the parameters into networks.yml

---
+++ update.sh
@@ -0,0 +1,1 @@
+#!/bin/bash                                 aws cloudformation update-stack --stack-name$1 --region us-east-1 --template-body file://$2 --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
